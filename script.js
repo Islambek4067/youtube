518ef9bf4bc3e009dc5b302c5188bb518d4611bd
Almas Islambek <i.almas4067@gmail.com>
2026-02-05 20:11:00 +0500

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const chipAll = document.querySelector('.chip-all')
-const profile = document.querySelector('.profile')
 const avatar = document.querySelector('.avatar')
 const createBtn = document.querySelector('.create-btn')
 const menuToggle = document.querySelector('#menuToggle')
@@ -8,19 +7,27 @@ const hiddenSideBar = document.querySelector('#hiddenSideBar')
 const chipGaming = document.querySelector('.chip-gaming')
 const chipMusic = document.querySelector('.chip-music')
 const chipPodcast = document.querySelector('.chip-podcast')
+const musicVideo = document.querySelector('.music-video')
 const chipJs = document.querySelector('.chip-js')
 const chipMixes = document.querySelector('.chip-mixes')
 const chipPlaylists = document.querySelector('.chip-playlists')
 const allVideo = document.querySelector('.all-video')
 const gamingVideo = document.querySelector('.gaming-video')
 const create = document.querySelector('#create')
+const AccountPanel = document.querySelector('#zx9-account-panel-root'
+)
+const podcastVideo = document.querySelector('.podcast-video')
 chipAll.addEventListener('click', ()=>{
   gamingVideo.classList.add('hidden')
   allVideo.classList.remove('hidden')
+   musicVideo.classList.add('hidden')
+  podcastVideo.classList.add('hidden')
 })
 chipGaming.addEventListener('click', ()=>{
   gamingVideo.classList.remove('hidden')
   allVideo.classList.add('hidden')
+  musicVideo.classList.add('hidden')
+  podcastVideo.classList.add('hidden')
 })
 menuToggle.addEventListener('click',()=>{
   hiddenSideBar.classList.remove('hidden')
@@ -35,5 +42,24 @@ createBtn.addEventListener('click', ()=>{
 
 
 avatar.addEventListener('click', ()=>{
-  profile.classList.toggle('hidden')
+  AccountPanel.classList.toggle('hidden')
+  if(!AccountPanel.classList.contains('hidden')){
+    create.classList.add('hidden')
+
+  }
+})
+
+chipMusic.addEventListener('click', ()=>{
+  gamingVideo.classList.add('hidden')
+  allVideo.classList.add('hidden')
+  musicVideo.classList.remove('hidden')
+  podcastVideo.classList.add('hidden')
+})
+
+
+chipPodcast.addEventListener('click', ()=>{
+  podcastVideo.classList.remove('hidden')
+  gamingVideo.classList.add('hidden')
+  allVideo.classList.add('hidden')
+  musicVideo.classList.add('hidden')
 })
